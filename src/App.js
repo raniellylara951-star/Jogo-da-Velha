41616e9f15ea45fb3bd5e97f93d6b17b02d436d7
Ana Clara Pimentel <4naclarapimentel@gmail.com>
2026-06-12 11:59:59 -0300

    Criar estrutura inicial do jogo da velha com tabuleiro e componentes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,57 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+import Square from "./components/Square";
+
+export default function App() {
+
+  const [squares, setSquares] =
+    useState(Array(9).fill(null));
+
+  function handleClick(i) {
+
+  if (squares[i]) return;
+
+  const nextSquares = [...squares];
+
+  nextSquares[i] =
+    xIsNext ? "X" : "O";
+
+  setSquares(nextSquares);
+
+  setXIsNext(!xIsNext);
 }
+
+
+  return (
+  <>
+    <div className="board-row">
+      <Square value={squares[0]} onClick={() => handleClick(0)} />
+      <Square value={squares[1]} onClick={() => handleClick(1)} />
+      <Square value={squares[2]} onClick={() => handleClick(2)} />
+    </div>
+
+    <div className="board-row">
+      <Square value={squares[3]} onClick={() => handleClick(3)} />
+      <Square value={squares[4]} onClick={() => handleClick(4)} />
+      <Square value={squares[5]} onClick={() => handleClick(5)} />
+    </div>
+
+    <div className="board-row">
+      <Square value={squares[6]} onClick={() => handleClick(6)} />
+      <Square value={squares[7]} onClick={() => handleClick(7)} />
+      <Square value={squares[8]} onClick={() => handleClick(8)} />
+    </div>
+  </>
+);
+}
+
+const [xIsNext, setXIsNext] =
+  useState(true);
+
+function restartGame() {
+  setSquares(Array(9).fill(null));
+  setXIsNext(true);
+}
+
+<button onClick={restartGame}>
+  Reiniciar Jogo
+</button>
