0c0b5445230a2b5c8437b5bb9146326befbda2aa
Giselle Carla <gisellecarla1909@gmail.com>
2026-07-17 09:56:15 -0300

    Implementação do histórico de jogadas

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ export default function App() {
   const [squares, setSquares] =
     useState(Array(9).fill(null));
 
+
     const [xIsNext, setXIsNext] =
   useState(true);
 
     function handleClick(i) {
 
+      if (winner || squares[i]) {
+        return;
+      }
       if (squares[i]) return;
     
       const nextSquares = [...squares];
@@ -25,6 +29,14 @@ export default function App() {
 
   return (
     <>
+    <div className="game">
+  
+      <h2> 
+        Vez do jogador:
+        {xINext ? "x" : "O" }
+         </h2>
+    </div>
+  
       <div className="board-row">
         <Square value={squares[0]} onClick={() => handleClick(0)} />
         <Square value={squares[1]} onClick={() => handleClick(1)} />
@@ -46,3 +58,43 @@ export default function App() {
   );
 
 }
+
+function calculateWinner(squares) {
+
+  const lines = [
+
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+
+    [0,4,8],
+    [2,4,6]
+
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+
+    const [a,b,c] = lines[i];
+
+    if (
+      squares[a] &&
+      squares[a] === squares[b] &&
+      squares[a] === squares[c]
+    ) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
+   const winner =
+   calculateWinner(Squares);
+    <h2>
+      {winner
+      ?`Vencedor: ${winner}`
+      :`Vez do jogador: ${xINext ? "x" : "O" }`}
+    </h2>
