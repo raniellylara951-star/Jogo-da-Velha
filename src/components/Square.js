afc8ccfccb26676290a232247ea17b3f788adcf7
Helena Albuquerque <halbuquerque259@gmail.com>
2026-07-20 15:36:54 -0300

    modificando o codigo Square e styles.css

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,18 +1,17 @@
-export default function Square({
-  value,
-  onClick,
-  isWinner
-}) {
-    return (
-      <button
-  className={`square ${isWinner ? "winner" : ""}`}
-  onClick={onClick}
->
-        {value}
-      </button>
-    );
-  }
-
- 
-
-  
+export default function Square({ value, onClick, isWinner }) {
+  return (
+    <button
+      className={`square ${isWinner ? "winner" : ""}`}
+      onClick={onClick}
+    >
+      {/* Renderização condicional: se houver Pokémon na casa, exibe sua imagem (Requisito 2) */}
+      {value?.image && (
+        <img 
+          src={value.image} 
+          alt={value.name} 
+          className="pokemon-sprite" 
+        />
+      )}
+    </button>
+  );
+}
